c462a08bdf3c73791c614319af348cec47ad390e
tjb09009 <timothy.becker@uconn.edu>
2018-08-02 12:55:30 -0400

    version updates

--- require.sh
+++ require.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+#cp data/ML_nodes/require.sh ./ && umount /media/data && chgrp root require.sh && chown root require.sh && chmod a+x require.sh && ./require.sh > install.log
+
 #build your R install of 3.5
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
-sed '$ a deb http://www.club.cc.cmu.edu/pub/ubuntu/ bionic-backports main restricted universe' /etc/apt/sources.list >> /etc/apt/sources.list
-sed '$ a deb https://cloud.r-project.org/bin/linux/ubuntu xenial-cran35/' /etc/apt/sources.list >> /etc/apt/sources.list
+sed '$ a deb http://www.club.cc.cmu.edu/pub/ubuntu/ bionic-backports main restricted universe' /etc/apt/sources.list
+sed '$ a deb https://cloud.r-project.org/bin/linux/ubuntu xenial-cran35/' /etc/apt/sources.list
 #modify /etc/environment PATH and SPARK_HOME
 
 #apt installs
@@ -18,6 +20,8 @@ git \
 maven \
 wget \
 nano \
+screen \
+docker.io \
 libffi6 \
 libffi-dev \
 libssl1.0.0 \
@@ -27,8 +31,8 @@ libblas-dev \
 liblapack3 \
 liblapack-dev \
 libcurl4-openssl-dev \
-libxml2-devel \
-libncurses-dev \
+libxml2-dev \
+libncurses5-dev \
 libbz2-dev \
 zlib1g-dev \
 r-base \
@@ -42,7 +46,7 @@ cd /software
 git clone https://github.com/mysql/mysql-connector-python.git
 cd mysql-connector-python
 python ./setup.py build
-sudo python ./setup.py install
+python ./setup.py install
 cd /
 
 #spark install setup
@@ -54,20 +58,23 @@ mv spark-2.3.1-bin-hadoop2.7 /opt/spark
 cd / 
 
 
+#cuda install setup from nvidia installers...
+
 #R installers
 Rscript --save -e "install.packages('sparklyr'); install.packages('ggplot2')"
 Rscript --save -e "source('https://bioconductor.org/biocLite.R'); biocLite('DNAcopy'); biocLite('cn.mops'); biocLite('ballgown');"
 
 #pip installers
 pip install -I argparse
-pip instakk -Iv 'cython>=0.24.0,<0.25.0'
-pip install -I numpy
-pip install -I scipy
-pip install -I scikit-learn
+pip install -Iv 'cython>=0.24.0,<0.25.0'
 pip install -Iv 'pyspark>2.3,<=2.3.1'
 pip install -Iv 'tensorflow>=1.9.0,<1.9.1'
-pip install -I chainer
+#tensorflow-gpu
+pip install -I  chainer
 pip install -Iv 'h5py>=2.8.0,<2.8.1'
+pip install -Iv --no-binary 'numpy>=1.15,<=1.16'
+pip install -I --no-binary scipy
+pip install -I --no-binary scikit-learn
 #pip install -I opencv3
 
 #remote and bio installs
