e8fd2c30e18808a325ea344a7e81aa96464d67a7
timothyjamesbecker <timothyjamesbecker@gmail.com>
2018-08-04 14:28:59 -0400

    R 3.5 deb working

--- require.sh
+++ require.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#cp data/ML_nodes/require.sh ./ && umount /media/data && chgrp root require.sh && chown root require.sh && chmod a+x require.sh && ./require.sh > install.log
+#cp data/ML_nodes/require.sh ./ && chgrp root require.sh && chown root require.sh && chmod a+x require.sh && ./require.sh > install.log
 
 #build your R install of 3.5
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
-sed '$ a deb http://www.club.cc.cmu.edu/pub/ubuntu/ bionic-backports main restricted universe' /etc/apt/sources.list
-sed '$ a deb https://cloud.r-project.org/bin/linux/ubuntu xenial-cran35/' /etc/apt/sources.list
-#modify /etc/environment PATH and SPARK_HOME
+sed -i '$ a deb http://lib.stat.cmu.edu/R/CRAN/bin/linux/ubuntu xenial-cran35/' /etc/apt/sources.list
+
+#deb https://cloud.r-project.org/bin/linux/ubuntu xenial-cran35/
 
 #apt installs
 apt-get update && apt-get install -y \
@@ -42,6 +42,7 @@ python-pip \
 && apt-get clean
 
 #mysql python interface
+mkdir /software
 cd /software
 git clone https://github.com/mysql/mysql-connector-python.git
 cd mysql-connector-python
@@ -56,7 +57,15 @@ tar -xzf spark-2.3.1-bin-hadoop2.7.tgz
 rm spark-2.3.1-bin-hadoop2.7.tgz
 mv spark-2.3.1-bin-hadoop2.7 /opt/spark
 cd / 
-
+#modify /etc/environment PATH and SPARK_HOME
+sed -i '$ a PATH=$PATH":/opt/spark/bin"' /etc/environment
+sed -i '$ a SPARK_HOME="/opt/spark"' /etc/environment
+export PATH=$PATH":/opt/spark/bin"
+export SPARK_HOME="/opt/spark"
+sed -i '$ a export PATH=$PATH":/opt/spark/bin"' ~/.bashrc
+sed -i '$ a export SPARK_HOME="/opt/spark"' ~/.bachrc
+echo 'PATH is now set to: '$PATH
+echo 'SPARK_HOME is now set to: '$SPARK_HOME
 
 #cuda install setup from nvidia installers...
 
@@ -68,14 +77,15 @@ Rscript --save -e "source('https://bioconductor.org/biocLite.R'); biocLite('DNAc
 pip install -I argparse
 pip install -Iv 'cython>=0.24.0,<0.25.0'
 pip install -Iv 'pyspark>2.3,<=2.3.1'
+pip install -Iv 'h5py>=2.8.0,<2.8.1'
+pip install -Iv 'numpy>=1.15,<=1.16'
+pip install -I scipy
+pip install -I scikit-learn
+#pip install -I opencv3
 pip install -Iv 'tensorflow>=1.9.0,<1.9.1'
 #tensorflow-gpu
 pip install -I  chainer
-pip install -Iv 'h5py>=2.8.0,<2.8.1'
-pip install -Iv --no-binary 'numpy>=1.15,<=1.16'
-pip install -I --no-binary scipy
-pip install -I --no-binary scikit-learn
-#pip install -I opencv3
+
 
 #remote and bio installs
 pip install -I paramiko
