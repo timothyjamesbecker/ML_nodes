88fc6c1e719196c2798d7b6edd3c04e9bc539670
timothyjamesbecker <timothyjamesbecker@gmail.com>
2018-12-14 14:05:19 -0500

    remote removed for more robust functionality and better monitoring

--- dispatch.py
+++ dispatch.py
@@ -16,7 +16,7 @@ import utils
 def get_resources(node,disk_patterns=['/','/data'],verbose=False,rounding=2):
     N = {node:{'cpu':0.0,'mem':0.0,'swap':0.0,'disks':{p:0.0 for p in disk_patterns}}}
     N[node]['err'] = {}
-    check = 'top -d 0.5 -n 5 | grep "Cpu" | tail -n 1 && '+\
+    check = 'top -n 5 | grep "Cpu" | tail -n 1 && '+\
             'top -n 1 | grep "KiB Mem" && top -n 1 | grep "KiB Swap"'
     check += ' && '+' && '.join(['df -h | grep %s'%p for p in disk_patterns])
     command = ["ssh %s -t '%s'"%(node,check)]
