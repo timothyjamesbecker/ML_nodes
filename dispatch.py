677de817665ba5f0937c8f6e070d9ef06fca8049
timothyjamesbecker <timothyjamesbecker@gmail.com>
2018-12-28 12:20:13 -0500

    core temp monitoring via sensors uid,pwd UX updates

--- dispatch.py
+++ dispatch.py
@@ -40,7 +40,7 @@ def get_resources(node,disk_patterns=['/','/data'],verbose=False,rounding=2):
         line = line.replace('\x1b',' ').replace('(B',' ').replace('[m',' ').replace('[1m',' ').replace('[',' ')
         line = re.sub(' +',' ',line)
         try:
-            if line.upper().startswith('%CPU(S)'):
+            if line.startswith('%Cpu(s)'):
                 cleaned        = float(line.split(',')[3].strip(' ').split(' ')[1])
                 idle_cpu       = round(100.0-cleaned,rounding)
                 N[node]['cpu'] = idle_cpu
@@ -48,7 +48,7 @@ def get_resources(node,disk_patterns=['/','/data'],verbose=False,rounding=2):
             N[node]['err']['cpu'] = E.message
             pass
         try:
-            if line.upper().startswith('KIB MEM'):
+            if line.startswith('KiB Mem'):
                 total_mem      = float(line.split(',')[0].strip(' ').split(' ')[4])
                 free_mem       = float(line.split(',')[1].strip(' ').split(' ')[1])
                 N[node]['mem'] = round(100.0*(1.0-free_mem/total_mem),rounding)
@@ -56,7 +56,7 @@ def get_resources(node,disk_patterns=['/','/data'],verbose=False,rounding=2):
             N[node]['err']['mem'] = E.message
             pass
         try:
-            if line.upper().startswith('KIB SWAP'):
+            if line.startswith('KiB Swap'):
                 total_swap      = float(line.split(',')[0].strip(' ').split(' ')[3])
                 free_swap       = float(line.split(',')[1].strip(' ').split(' ')[1])
                 N[node]['swap'] = round(100.0-100.0*(free_swap/total_swap),rounding)
@@ -73,9 +73,9 @@ def get_resources(node,disk_patterns=['/','/data'],verbose=False,rounding=2):
             N[node]['err']['disks'] = E.message
             pass
         try:
-            if line.upper().startswith('CORE'):
+            if line.startswith('Core'):
                 core = line.replace('\r','').replace('\n','')
-                core_numb = int(line.split(':')[0].replace(' ','').upper().split('CORE')[-1])
+                core_numb = int(line.split(':')[0].replace(' ','').split('Core')[-1])
                 core_temp = float(re.sub(' +',' ',line).split(':')[-1].strip(' ').split(' ')[0].split('C')[0])
                 if 'core_temp' not in N[node]:
                     N[node]['core_temp'] = {core_numb:core_temp}
@@ -86,13 +86,13 @@ def get_resources(node,disk_patterns=['/','/data'],verbose=False,rounding=2):
             pass
     if N[node]['err'] != {}: N[node]['err']['out'] = R['out']
     else:                    N[node].pop('err')
-    if 'core_temp' in N[node]:
-        x,ks = 0.0,N[node].keys()
-        for k in ks:
-            x += N[node]['core_temp'][k]
-            N[node].pop(k)
-        if len(ks)>0: x /= len(ks)
-        N[node]['core_temp'] = x
+    # if 'core_temp' in N[node]:
+    #     x,ks = 0.0,N[node].keys()
+    #     for k in ks:
+    #         x += N[node]['core_temp'][k]
+    #         N[node].pop(k)
+    #     if len(ks)>0: x /= len(ks)
+    #     N[node]['core_temp'] = x
     return {'status':N}
 
 #[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[
